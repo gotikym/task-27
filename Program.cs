7e87bb22ac22b6223da8428646d7d89f921692c6
gotikym <gotikym9@mail.ru>
2022-06-03 11:37:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string enteredText = "Любое сочетание слов, каждое слово выводим с новой строки";
+        string[] texts = enteredText.Split(' ', ',');
+
+        foreach (var text in texts)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
